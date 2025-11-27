b17475cd934fb5c2948e46291d900beae9b311e5
Tigana <tigana137@gmail.com>
2025-11-27 18:30:10 +0100

    addin front end to test open api

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Home from "./pages/Home";
-import SignUp from "./pages/SignUp";
-import NotFound from "./pages/NotFound";
-import AuthenticatedRoutes from "./guard/AuthenticatedRoutes";
-import Profile from "./pages/Profile";
-import { AuthProvider } from "./context/AuthContext";
-import SignIn from "./components/SignIn/SignIn";
-import NetworkStatusGuard from "./guard/NetworkStatusGuard";
-import { Toaster } from "sonner";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import NotFound from './pages/NotFound';
+import AuthenticatedRoutes from './guard/AuthenticatedRoutes';
+import Profile from './pages/Profile';
+import { AuthProvider } from './context/AuthContext';
+import SignIn from './components/SignIn/SignIn';
+import NetworkStatusGuard from './guard/NetworkStatusGuard';
+import { Toaster } from 'sonner';
 
 const queryClient = new QueryClient();
 
